079108f62f5ca070e967a5c7b968ca848ab0fe44
tahmid <mstahmid0@gmail.com>
2024-10-20 03:36:06 -0500

    called functions to test

--- bro-code/main.py
+++ bro-code/main.py
@@ -1,11 +1,19 @@
 import threading
+import time
 
-def wakl_dong():
+def wakl_dog():
+	time.sleep(5);
 	print("walking the dog done")
 
 def trash():
+	time.sleep(3)
 	print("throwing trash done")
 
 def mail():
+	time.sleep(2)
 	print("mail check done")
 
+
+walk_dog()
+trash()
+mail()
